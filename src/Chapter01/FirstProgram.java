6cac22a7e31415dac48d05288f4908a5128bd8cc
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-11-21 23:03:49 -0500

    Code added

--- src/Chapter01/FirstProgram.java
+++ src/Chapter01/FirstProgram.java
@@ -14,3 +14,24 @@ public class FirstProgram {
         System.out.println("2 + 2 is " + answer);
     }
 }
+
+/*
+What is a compiler?
+A compiler translates a code into a low-level language. When you compile a java program, the
+compiler translates your code into a program expressed in java bytecode.
+
+What is a source program?
+The program that is input to a compiler.
+
+What is an object?
+The translated program that is produced by the compiler.
+
+What you call the program that runs Java byte-code instructions?
+Program that runs java byte-code instructions is called an interpreter. It is often called a JVM.
+
+You define a class named NiceClass in a file. What name should the file have?
+NiceClass.java
+
+You compile the class NiceClass. What will be the name of the file with resulting byte-code?
+NiceClass.class
+ */
---
+++ src/Chapter01/Variables.java
@@ -0,0 +1,5 @@
+package Chapter01;
+
+public class Variables {
+    // Left work at page 14 midPage
+}
