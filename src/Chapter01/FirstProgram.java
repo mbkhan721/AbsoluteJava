92a4e9aa0cb25e1fb532e197ee38a903f7abf473
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-11-21 22:24:49 -0500

    Project start

---
+++ src/Chapter01/FirstProgram.java
@@ -0,0 +1,16 @@
+package Chapter01;
+
+public class FirstProgram {
+
+    public static void main(String[] args) { // Begin the program named FirstProgram
+        System.out.println("Hello reader."); // Output what is shown in the parenthesis
+        System.out.println("Welcome to Java!");
+
+        // System.out is an object used for sending output to the screen
+        // println is the method (that is, the action) that this object performs.
+        System.out.println("Let's demonstrate a simple calculation.");
+        int answer;
+        answer = 2 + 2;
+        System.out.println("2 + 2 is " + answer);
+    }
+}
