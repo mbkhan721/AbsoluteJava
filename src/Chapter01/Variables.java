f90fbe4e659427d5f28c3ed8deabae56f392e59f
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-12-17 00:19:25 -0500

    Code added

--- src/Chapter01/Variables.java
+++ src/Chapter01/Variables.java
@@ -2,4 +2,20 @@ package Chapter01;
 
 public class Variables {
     // Left work at page 14 midPage
+
+    public static void main(String[] args) {
+        double distance = 1.5;
+        int time = (int) (distance * 80);
+
+        double balance = 10;
+        double rate = 0.3;
+        double interest = balance * rate;
+    }
 }
+
+
+
+
+
+
+
