8fc0b67cec7cc1f88c3ca2349abd72bbe7d9471d
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2022-08-04 23:47:13 -0400

    Code added

--- src/Chapter01/Strings.java
+++ src/Chapter01/Strings.java
@@ -19,10 +19,29 @@ public class Strings {
 
         String greet = "Hi Mary!";
         System.out.println("\nPrinting greet in all lower case: " + greet.toLowerCase(Locale.ROOT));
+
+        String pause = " Hmm ";
+        System.out.println("\nString trim() removes leading and trailing white space:" + pause.trim());
+
+        System.out.println("\ncharAt(0): " + greeting.charAt(0));
+        System.out.println("charAt(1): " + greeting.charAt(1));
+
+        String sample = "AbcdefG";
+        System.out.println("\nsubstring(start): " + sample.substring(2));
+        System.out.println("substring(start, end): " + sample.substring(2, 5));
+
+        System.out.println("\nindexOf(first occurrence): " + greet.indexOf("Mary"));
+        System.out.println("indexOf(first occurrence): " + greet.indexOf("Sally"));
+
+        System.out.println("\nlastIndexOf(Hi Mary!): " + greet.lastIndexOf("r"));
+
+        String myName = "Muhammad";
+        myName = myName + " Khan";
+        System.out.println("\n" + myName);
     }
 }
 
-// Left work at page 39 toLowerCase()
+// Begin work at page 46
 
 
 
