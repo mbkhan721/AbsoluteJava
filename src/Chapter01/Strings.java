ae0b66cc30acf447d8082fa96b45c1b5751dcb0a
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2022-08-04 23:03:03 -0400

    Code added

--- src/Chapter01/Strings.java
+++ src/Chapter01/Strings.java
@@ -1,5 +1,7 @@
 package Chapter01;
 
+import java.util.Locale;
+
 public class Strings {
 
     public static void main(String[] args) {
@@ -15,6 +17,8 @@ public class Strings {
         String name = "mary!";
         System.out.println("\nmary equalsIgnoreCase Mary? " + name.equalsIgnoreCase("Mary!"));
 
+        String greet = "Hi Mary!";
+        System.out.println("\nPrinting greet in all lower case: " + greet.toLowerCase(Locale.ROOT));
     }
 }
 
